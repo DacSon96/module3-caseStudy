747087eaf5d5eed911022997cc430eff9672244b
hanhbyte <hell.boyx229@gmail.com>
2021-10-06 23:31:31 +0700

    Create login page

--- src/main/java/controller/LoginServlet.java
+++ src/main/java/controller/LoginServlet.java
@@ -1,25 +1,17 @@
 package controller;
 
-import dao.customer.CustomerDAO;
 import java.io.IOException;
-import java.io.PrintWriter;
 import java.io.UnsupportedEncodingException;
-import java.sql.Connection;
-import java.sql.DriverManager;
-import java.sql.PreparedStatement;
-import java.sql.ResultSet;
-import java.sql.SQLException;
-import java.util.List;
 import javax.servlet.RequestDispatcher;
 import javax.servlet.ServletException;
 import javax.servlet.annotation.WebServlet;
 import javax.servlet.http.HttpServlet;
 import javax.servlet.http.HttpServletRequest;
 import javax.servlet.http.HttpServletResponse;
-import javax.servlet.http.HttpSession;
 import model.Customer;
 import service.customer.CustomerService;
 import service.customer.ICustomerService;
+import validate.Validate;
 
 @WebServlet(name = "LoginServlet", value = "/login")
 public class LoginServlet extends HttpServlet {
@@ -37,8 +29,8 @@ public class LoginServlet extends HttpServlet {
       action = "";
     }
     switch (action) {
-      case "signup":
-        signup(request, response);
+      case "registration":
+        registration(request, response);
         break;
       case "login":
         login(request, response);
@@ -54,8 +46,8 @@ public class LoginServlet extends HttpServlet {
       action = "";
     }
     switch (action) {
-      case "signup":
-        signup(request, response);
+      case "registration":
+        registration(request, response);
         break;
       case "login":
         login(request, response);
@@ -64,40 +56,41 @@ public class LoginServlet extends HttpServlet {
   }
 
 
-  private void signup(HttpServletRequest request, HttpServletResponse response)
+  private void registration(HttpServletRequest request, HttpServletResponse response)
       throws UnsupportedEncodingException {
-//    response.setContentType("text/html;charset=UTF-8");
-//    request.setCharacterEncoding("utf-8");
-//    RequestDispatcher dispatcher = request.getRequestDispatcher(
-//        "/homepage?action=&username=&password=");
-//    String name = request.getParameter("name");
-//    String phone = request.getParameter("phone");
-//    String username = request.getParameter("username");
-//    String password = request.getParameter("password");
-//    boolean status = true;
-//    Customer customer = new Customer(name,
-//        phone,
-//        email,
-//        password,
-//        status);
-//    String message;
-//    Validate validate = Validate.getInstance();
-//    if (validate.validate(name, validate.regexEmail)
-//        && validate.validate(phone, validate.regexPhone)
-//        && validate.validate(password, validate.regexPassword)) {
-//      customer.save(customer);
-//      request.setAttribute("message", message);
-//    } else {
-//      request.setAttribute("message", message);
-//      request.setAttribute("customer", customer);
-//    }
-//    try {
-//      dispatcher.forward(request, response);
-//    } catch (ServletException e) {
-//      e.printStackTrace();
-//    } catch (IOException e) {
-//      e.printStackTrace();
-//    }
+    response.setContentType("text/html;charset=UTF-8");
+    request.setCharacterEncoding("utf-8");
+    RequestDispatcher dispatcher = request.getRequestDispatcher("/homepage?action=&username=&password=");
+    int id = request.getIntHeader("id");
+    String name = request.getParameter("name");
+    int age = request.getIntHeader("age");
+    String phone = request.getParameter("phone");
+    String address = request.getParameter("address");
+    String email = request.getParameter("email");
+    String username = request.getParameter("username");
+    String password = request.getParameter("password");
+    String role = request.getParameter("role");
+    Customer customer = new Customer(id, name, age, phone, address , email, username, password, role);
+    String message;
+    Validate validate = Validate.getInstance();
+    if (validate.validate(username, validate.regexEmail)
+        && validate.validate(phone, validate.regexPhone)
+        && validate.validate(password, validate.regexPassword)) {
+      customerService.save(customer);
+      message = "Sign Up Success!!";
+      request.setAttribute("message", message);
+    } else {
+      message = "Registration failed, please re-enter!";
+      request.setAttribute("message", message);
+      request.setAttribute("customer", customer);
+    }
+    try {
+      dispatcher.forward(request, response);
+    } catch (ServletException e) {
+      e.printStackTrace();
+    } catch (IOException e) {
+      e.printStackTrace();
+    }
   }
 
   private void login(HttpServletRequest request, HttpServletResponse response)
--- src/main/java/validate/Validate.java
+++ src/main/java/validate/Validate.java
@@ -5,9 +5,14 @@ import java.util.regex.Pattern;
 
 public class Validate {
   private static Validate instance;
+  public static String regexId = "^[0-9]{9,10}$";
+  public static String regexName = "^[0-9]{9,10}$";
+  public static String regexAge = "^[0-9]{9,10}$";
   public static String regexPhone = "^[0-9]{9,10}$";
+  public static String regexAddress = "^[0-9]{9,10}$";
   public static String regexPassword = "^[0-9]{6,8}$";
   public static String regexEmail = "^[\\w]{4,8}$";
+
 
   private Validate() {
   }
