3ff28691978e50a75cfee562dec731349f829ec6
truc-nguyenminh <truc.nguyenminh93@gmail.com>
2021-10-08 16:55:38 +0700

    truc: fix bug admin

--- src/main/java/dao/cart/CartDao.java
+++ src/main/java/dao/cart/CartDao.java
@@ -21,7 +21,7 @@ public class CartDao implements ICartDao {
     @Override
     public boolean create(Cart cart) {
         try {
-            PreparedStatement statement = connection.prepareStatement("INSERT INTO case_study_module_3.cart (productId, quatity) VALUES ( ?, ?)");
+            PreparedStatement statement = connection.prepareStatement("INSERT INTO cart (productId, quantity) VALUES ( ?, ?)");
             statement.setInt(1, cart.getProductId());
             statement.setInt(2, cart.getQuantity());
 
